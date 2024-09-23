5d2624e3f31dff0e6858723c879fabfb235e0ddc
samzhao <samzhao@bu.edu>
2024-09-22 23:05:32 -0400

    version6

--- cs412/settings.py
+++ cs412/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 
 ## tell Django about our static resources directory:
 import os # operating system library
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "quotes/static")
 ]
 
 
